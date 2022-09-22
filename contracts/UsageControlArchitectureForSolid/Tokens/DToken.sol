ec5211881d2edfb099c45d731c5229c24e980653
Remix Workspace <>
2022-09-22 13:17:20 +0200

    commit TOKENIZATION

---
+++ contracts/UsageControlArchitectureForSolid/Tokens/DToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../Libraries/Ownable.sol";
+
+contract DToken is ERC20,Ownable {
+
+    constructor() ERC20("DToken","DT"){}  
+
+
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) 
+    {
+        _mint(account, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public onlyOwner virtual 
+    {
+        _burn(_msgSender(), amount);
+    }
+
+
+    
+
+}
